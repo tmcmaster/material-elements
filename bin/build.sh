160fcb10557393d60e70590f3c93dcfe903d3daf
Tim McMaster <tim@mcmaster.id.au>
2019-11-28 20:31:58 +1100

    checkpoint

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,9 @@
 npm install
 polymer build
 rsync -a --exclude polymer build/default/node_modules/@material/ src/
+#rsync -a build/default/node_modules/blocking-elements/ src/blocking-elements/
+#rsync -a build/default/node_modules/tslib/ src/tslib/
+#rsync -a build/default/node_modules/wicg-inert/ src/wicg-inert/
 
 for i in `grep -rl '\.\./tslib/' src`; do perl -spi -e 's/(\.\.\/)+tslib/tslib/g' "$i"; done
 for i in `grep -rl '\.\./blocking-elements/' src`; do perl -spi -e 's/(\.\.\/)+blocking-elements/blocking-elements/g' "$i"; done
