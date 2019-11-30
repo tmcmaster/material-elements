f7242ae786597c41ba93abc87e6d21d67629c296
Tim McMaster <tim@mcmaster.id.au>
2019-11-30 13:25:40 +1100

    created a component for building demos.

--- docs/main.js
+++ docs/main.js
@@ -1,55 +1,47 @@
 import {html, render} from "./web_modules/lit-html.js";
 
+import './tm-demo.js';
+
 render(html`
     <style>
         body {
-          background-color: lightgray;
-          padding: 0;
-          margin: 0;
-          display: flex;
-          flex-direction: row;
-          justify-content: center;
-        } 
-        
-        div {
-            
-        }
-        
-        h3 {
-            width: 100%;
-            text-align: left;
+            background: lightgray;
         }
     </style>
-    <div>
-        <h3>Material Elements</h3>
-        <mwc-button>MWC Button</mwc-button>
-        <mwc-textfield label="label"></mwc-textfield>
-        
-        <div>
-          <mwc-checkbox></mwc-checkbox>
-          <mwc-checkbox checked></mwc-checkbox>
-          <mwc-checkbox indeterminate></mwc-checkbox>
-        </div>
-        
-        <div>
-          <mwc-checkbox disabled></mwc-checkbox>
-          <mwc-checkbox disabled checked></mwc-checkbox>
-          <mwc-checkbox disabled indeterminate></mwc-checkbox>
-        </div>
-        
-        <div>
-          <style>
-            mwc-checkbox.pink {
-              --mdc-theme-secondary: #e91e63;
-            }
-          </style>
-          <mwc-checkbox class="pink"></mwc-checkbox>
-          <mwc-checkbox class="pink" checked></mwc-checkbox>
-          <mwc-checkbox class="pink" indeterminate></mwc-checkbox>
-        </div>
-    </div>
+    <tm-demo heading="Material Elements">
     
-    
+        <section title="MWC Button">
+             <style>
+                mwc-button {
+                    width: 100%;
+                }
+            </style>
+            <mwc-button>MWC Button</mwc-button> 
+        </section>
+        
+        <section title="MWC Checkbox">
+            <div>
+              <mwc-checkbox></mwc-checkbox>
+              <mwc-checkbox checked></mwc-checkbox>
+              <mwc-checkbox indeterminate></mwc-checkbox>
+            </div>
+            <div>
+              <mwc-checkbox disabled></mwc-checkbox>
+              <mwc-checkbox disabled checked></mwc-checkbox>
+              <mwc-checkbox disabled indeterminate></mwc-checkbox>
+            </div>
+            <div>
+              <style>
+                mwc-checkbox.pink {
+                  --mdc-theme-secondary: #e91e63;
+                }
+              </style>
+              <mwc-checkbox class="pink"></mwc-checkbox>
+              <mwc-checkbox class="pink" checked></mwc-checkbox>
+              <mwc-checkbox class="pink" indeterminate></mwc-checkbox>
+            </div>
+        </section>
+        
+    </tm-demo>
     
 `, document.querySelector('body'));
-
